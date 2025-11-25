7527670da5962ccf740d9564396fc060261cd80e
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-11-25 13:38:34 +0100

    Updated boolean.asm

--- assembly-code/windows/boolean.asm
+++ assembly-code/windows/boolean.asm
@@ -1,6 +1,25 @@
+[32 bits x86]
 .data
     false equ 0
     true equ 255
-
+    
+.code
+    
+    
+    
+[64 bits x86]
+.data
+    false equ 0
+    true equ 255
+    
+.code
+    
+    
+    
+[128 bits x86]
+.data
+    false equ 0
+    true equ 255
+    
 .code
     
