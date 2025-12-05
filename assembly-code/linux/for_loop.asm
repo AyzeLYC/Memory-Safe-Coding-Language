9e359a9e7f8ca72f6ecf966fa6785bdd71be9bff
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2025-12-05 08:27:59 +0100

    Created for_loop.asm

---
+++ assembly-code/linux/for_loop.asm
@@ -0,0 +1,68 @@
+[ ARM 32 bits asm ]
+section .data
+
+section .text
+
+[ ARM 64 bits asm ]
+section .data
+
+section .text
+
+[ ARM 128 bits asm ]
+section .data
+
+section .text
+
+
+
+[ RISCV 16 bits asm ]
+section .data
+
+section .text
+
+[ RISCV 32 bits asm ]
+section .data
+
+section .text
+
+[ RISCV 64 bits asm ]
+section .data
+
+section .text
+
+[ RISCV 128 bits asm ]
+section .data
+
+section .text
+
+[ RISCV 256 bits asm ]
+section .data
+
+section .text
+
+
+
+[ x86 16 bits asm ]
+section .data
+
+section .text
+
+[ x86 32 bits asm ]
+section .data
+
+section .text
+
+[ x86 64 bits asm ]
+section .data
+
+section .text
+
+[ x86 128 bits asm ]
+section .data
+
+section .text
+
+[ x86 256 bits asm ]
+section .data
+    
+section .text
